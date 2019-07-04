5c8e8b7c53da450f63ad9d491f3a45b7d567a89c
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-07-04 23:00:31 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+
+
+echo "Sedang Menginstall Bahan Dulu"
+sleep 2
+apt update && apt upgrade -y
+pkg install python -y
+pkg install python2 -y
+pip install  --upgrade pip
+pip2 install requests mechanize
+
+pkg install curl
+
+clear
+
+echo "Installing selesai"
+
+
